eb8bbfcd6e0037206d63f5c43f0f5d4997e66ffd
KashCo26 <guptakashvi26@gmail.com>
2026-09-08 10:19:22 -0700

    Kashvi - startingNumber and doubling

--- NumbersRiddle.java
+++ NumbersRiddle.java
@@ -1,5 +1,9 @@
 public class NumbersRiddle{
     public static void main(String[] args){
-        System.out.println("Hello World");
+        int startingNumber = 5;
+        double numberDoubled = startingNumber * 2;
+        
+        System.out.println("Starting number: " + startingNumber);
+        System.out.println("Number doubled: " + numberDoubled);
     }
 }
