3fc952dd734e70598cac551b5cd8749da9a08143
pavik11 <ptgkalra@gmail.com>
2026-09-08 10:26:24 -0700

    Pavi - added 6 and divided by 2

--- NumbersRiddle.java
+++ NumbersRiddle.java
@@ -3,7 +3,13 @@ public class NumbersRiddle{
         int startingNumber = 5;
         double numberDoubled = startingNumber * 2;
         
-        System.out.println("Starting number: " + startingNumber);
-        System.out.println("Number doubled: " + numberDoubled);
+        System.out.println("Starting Number: " + startingNumber);
+        System.out.println("Number Doubled: " + numberDoubled);
+
+        double addSix = numberDoubled + 6;
+        System.out.println("Add 6: " + addSix);
+
+        double divideHalf = addSix / 2;
+        System.out.println("Divide by 2: " + divideHalf);
     }
 }
