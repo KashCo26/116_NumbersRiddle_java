f9dd6d3bda4a17eb306f462a2af0b3d40edbd727
RachitaChaudhari <rachita.c29@gmail.com>
2026-09-10 08:44:06 -0700

    Rachita - Finished Code

--- NumbersRiddle.java
+++ NumbersRiddle.java
@@ -1,15 +1,22 @@
 public class NumbersRiddle{
     public static void main(String[] args){
         int startingNumber = 5;
+        // Multiply by 2
         double numberDoubled = startingNumber * 2;
         
         System.out.println("Starting Number: " + startingNumber);
         System.out.println("Number Doubled: " + numberDoubled);
 
+        // Add 6
         double addSix = numberDoubled + 6;
         System.out.println("Add 6: " + addSix);
 
+        // Divide by 2
         double divideHalf = addSix / 2;
         System.out.println("Divide by 2: " + divideHalf);
+
+        // Subtract Starting Number
+        double subtractStarting = divideHalf - startingNumber;
+        System.out.println("Subtract Starting Number: " + subtractStarting);
     }
 }
